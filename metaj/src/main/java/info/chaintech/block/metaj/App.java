d80d1685a9937012bb49462bab31dbe4e5eea0a5
shniu <niushaohan@digcredit.com>
2018-07-28 17:03:54 +0800

    update

---
+++ metaj/src/main/java/info/chaintech/block/metaj/App.java
@@ -0,0 +1,12 @@
+package info.chaintech.block.metaj;
+
+/**
+ * 入口
+ *
+ * @author shniu
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
