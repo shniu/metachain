c1ebc6ddd68c757a7c32d635545ec01d0d4166d7
shniu <niushaohan@digcredit.com>
2018-08-06 10:04:14 +0800

    add block and hash util

--- metaj/src/main/java/info/chaintech/block/metaj/utils/HashUtil.java
+++ metaj/src/main/java/info/chaintech/block/metaj/utils/HashUtil.java
@@ -1,8 +1,14 @@
 package info.chaintech.block.metaj.utils;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Hex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.Security;
 import java.util.Objects;
 
 /**
@@ -12,6 +18,8 @@ import java.util.Objects;
  */
 
 public class HashUtil {
+
+    private static final Logger log = LoggerFactory.getLogger(HashUtil.class);
 
     /**
      * sha256 哈希算法
@@ -25,22 +33,48 @@ public class HashUtil {
 
             // 二进制 hash
             byte[] hash = digest.digest(data.getBytes("UTF-8"));
-
-            // 二进制转 16 进制
-            StringBuilder stringBuilder = new StringBuilder();
-            for (byte aHash : hash) {
-                String tmp = Integer.toHexString(aHash & 0xFF);
-                if (tmp.length() == 1) {
-                    stringBuilder.append("0");
-                }
-                stringBuilder.append(tmp);
-            }
-
-            return stringBuilder.toString();
+            return byteToHex(hash);
         } catch (UnsupportedEncodingException | NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            log.error(e.getMessage(), e);
         }
 
         return null;
     }
+
+    /**
+     * RipeMD160
+     */
+    public static String ripeMD160(String data) {
+        try {
+            Security.addProvider(new BouncyCastleProvider());
+            MessageDigest ripeMD160 = MessageDigest.getInstance("RipeMD160");
+            byte[] hash = ripeMD160.digest(data.getBytes("UTF-8"));
+            return Hex.toHexString(hash);
+        } catch (UnsupportedEncodingException | NoSuchAlgorithmException e) {
+            log.error(e.getMessage(), e);
+        }
+
+        return null;
+    }
+
+    /**
+     * 二进制转 16 进制
+     *
+     * @param hash 字节数组
+     * @return 16进制字符串
+     */
+    private static String byteToHex(byte[] hash) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (byte aHash : hash) {
+            String tmp = Integer.toHexString(aHash & 0xFF);
+            if (tmp.length() == 1) {
+                stringBuilder.append("0");
+            }
+            stringBuilder.append(tmp);
+        }
+
+        return stringBuilder.toString();
+    }
+
+
 }
