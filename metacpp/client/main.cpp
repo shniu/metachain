268255924f2108634da3d2ea9e7f704fe7f4e87d
shniu <niushaohan@digcredit.com>
2018-09-01 18:18:33 +0800

    add boost asio client

--- metacpp/client/main.cpp
+++ metacpp/client/main.cpp
@@ -4,26 +4,66 @@
 
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/array.hpp>
 
 using namespace std;
 
-using boost::system::error_code;
 using boost::asio::io_service;
 using boost::asio::ip::tcp;
 using boost::asio::ip::address;
 
-void connect_handler(const error_code &ec);
+void connect_handler(const boost::system::error_code &ec);
 
-int main(int argc, char** argv) {
+// asio_usage
+void asio_usage() {
     io_service service;
     tcp::endpoint endpoint(address::from_string("127.0.0.1"), 4000);
     tcp::socket socket(service);
     socket.async_connect(endpoint, connect_handler);
+    service.run();
+}
 
+// sync asio client to get daytime from the server
+void asio_sync_cli(int argc, char** argv) {
+
+    try {
+        io_service io;
+
+        /*tcp::resolver resolver(io);
+        tcp::resolver::query query(argv[1], "daytime");
+        tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);*/
+        tcp::endpoint endpoint(address::from_string("127.0.0.1"), 4001);
+
+        tcp::socket sock(io);
+        sock.connect(endpoint);
+        // boost::asio::connect(sock, endpoint);
+
+        for (;;) {
+            boost::array<char, 128> buf;
+            boost::system::error_code error;
+
+            size_t len = sock.read_some(boost::asio::buffer(buf), error);
+            if (error == boost::asio::error::eof) {
+                break;
+            } else if (error) {
+                throw boost::system::system_error(error);
+            }
+
+            cout.write(buf.data(), len);
+        }
+    } catch (exception &e) {
+        cout << e.what() << endl;
+    }
+
+}
+
+int main(int argc, char** argv) {
+    // asio_usage();
+    asio_sync_cli(argc, argv);
     return 0;
 }
 
-void connect_handler(const error_code &ec) {
+void connect_handler(const boost::system::error_code &ec) {
     cout << "connect succeed. \n"
          << ec.message()
          << endl;
--- metacpp/include/network/connection.h
+++ metacpp/include/network/connection.h
@@ -5,14 +5,23 @@
 #ifndef METACHAIN_CONNECTION_H
 #define METACHAIN_CONNECTION_H
 
+#include <boost/asio.hpp>
 #include <boost/enable_shared_from_this.hpp>
 
 namespace metachain {
 
+    using boost::asio::ip::tcp;
+    using boost::asio::io_service;
+
     // This class represents a connection to one other peer.
     class Connection : public boost::enable_shared_from_this<Connection> {
+    public:
+        Connection(io_service &io);
+        ~Connection();
 
-    };
+    private:
+        tcp::socket _socket;
+};
 
 }  // metachain
 
